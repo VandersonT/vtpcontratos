9d215fa547e9f50375fbe9678b71e0c15fb67906
VandersonThiago <vandersoncursos@gmail.com>
2021-07-21 19:30:06 -0300

    Criado o layout da  pagina 404

--- src/views/pages/404.php
+++ src/views/pages/404.php
@@ -1,1 +1,25 @@
-<h1>Páginaee não encontrada</h1>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina não encontrada</title>
+    <link rel="shortcut icon" type="image-x/png" href="<?=$base;?>/icon.ico">
+    <link rel="stylesheet" href="<?=$base;?>/assets/css/style.css" />
+    <link rel="stylesheet" href="<?=$base;?>/assets/css/404.css" />
+
+    <script src="https://kit.fontawesome.com/90bf9437da.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    
+    <section class="screen">
+        <div class="box">
+            <h1><i class="fas fa-bomb"></i> Vish deu problema</h1>
+            <p>A página que você tentou acessar não foi encontrada ou esta indisponivel no momento.</p>
+        </div>
+        <a href="javascript:history.go(-1)">Voltar</a>
+    </section>
+
+</body>
+</html>
