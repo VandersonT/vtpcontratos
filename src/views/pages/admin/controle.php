ead2d89410af1353ab5e8566d28ef31a3c7e06cb
VandersonThiago <vandersoncursos@gmail.com>
2021-07-24 16:53:27 -0300

    Criado o layout da tela de controle

--- src/views/pages/admin/controle.php
+++ src/views/pages/admin/controle.php
@@ -25,7 +25,79 @@
         <div class="btnMenuMobile"><i class="fas fa-caret-square-right"></i></div>
 
         <article class="body">
-            Resto do corpo
+            <h1>
+                <i class="fas fa-home"></i>
+                Painel de Controle - Gerador de Contratos
+            </h1>
+            <div class="box-infoGeneral">
+                <div class="infoSingle orange">
+                    <p>Usuários Online</p>
+                    <span>0</span>
+                </div>
+                <div class="infoSingle darkRed">
+                    <p>Total de Visitas</p>
+                    <span>0</span>
+                </div>
+            </div>
+
+            <br/><br/>
+
+            <h1>
+                <i class="fab fa-telegram-plane"></i>
+                Administradores
+            </h1>
+            <div class="box-userOn">
+                <div class="users">
+                    <div class="titles blue">
+                        <p>ID</p>
+                        <p>NOME</p>
+                        <p>CARGO</p>
+                    </div>
+                    <div class="userSingle">
+                        <p>1</p>
+                        <p>Vanderson</p>
+                        <p>Administrador</p>
+                    </div>
+                    <div class="userSingle">
+                        <p>2</p>
+                        <p>Gleison</p>
+                        <p>Ajudante</p>
+                    </div>
+                </div>
+            </div>
+
+            <br/><br/>
+
+            <h1>
+                <i class="fab fa-telegram-plane"></i>
+                Usuários onlines no site
+            </h1>
+            <div class="box-userOn">
+                <div class="users">
+                    <div class="titles ciano">
+                        <p>ID</p>
+                        <p>NOME</p>
+                        <p>ÚLTIMA AÇÃO</p>
+                    </div>
+                    <div class="userSingle">
+                        <p>1</p>
+                        <p>Bryan</p>
+                        <p>20-07-2021 18:00</p>
+                    </div>
+                    <div class="userSingle">
+                        <p>2</p>
+                        <p>Antonia</p>
+                        <p>20-07-2021 18:00</p>
+                    </div>
+                    <div class="userSingle">
+                        <p>3</p>
+                        <p>Marcos</p>
+                        <p>20-07-2021 18:00</p>
+                    </div>
+                </div>
+                
+            </div>
+
         </article>
     </section>
 
