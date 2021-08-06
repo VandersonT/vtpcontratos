fb3a093b114ef6a046c7548388e22bc155e8830a
VandersonThiago <vandersoncursos@gmail.com>
2021-08-06 11:55:13 -0300

    Acrescentado o meta que impedi o usuário de dar zoom na tela de qualquer jeito

--- src/views/pages/contracts/wedding1.php
+++ src/views/pages/contracts/wedding1.php
@@ -5,7 +5,7 @@
 
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width,height=device-height, target-densitydpi=device-dpi" />
     <meta name="description" content="Crie seus contratos muito mais rápido e mais profissional, acesse agora." />
     <meta name="keyword" content="contratos,gerador,geradordecontrato,contratos,modelocontrato,contract,contracts" />
     <meta name="author" content="VandersonT"/>
